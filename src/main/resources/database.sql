fbd3d303fa00734aca47b27432bfdb1b80670f5f
VladPrn <vladprn96@gmail.com>
2017-06-27 12:07:20 +0400

    SQL fix

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -128,6 +128,7 @@ INSERT INTO users VALUES (41, 'username40', '40my.world@mail.com', '$2a$11$uSXS6
 
 INSERT INTO roles VALUES (1, 'ROLE_USER');
 INSERT INTO roles VALUES (2, 'ROLE_ADMIN');
+INSERT INTO roles VALUES (3, 'ROLE_BlOCKED');
 
 INSERT INTO user_roles VALUES (1, 2);
 INSERT INTO user_roles VALUES (2, 1);
